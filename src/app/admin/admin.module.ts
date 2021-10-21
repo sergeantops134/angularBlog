59a66d95dc453f0107f32e2b8976d39e73a35c00
Olenych <p.olenych@andersenlab.com>
2021-10-21 18:12:47 +0300

    L-009 #comment Added auth service

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,6 +7,8 @@ import { DashboardPageComponent } from './dashboard-page/dashboard-page.componen
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AuthService} from "./shared/services/auth.service";
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
@@ -21,6 +23,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
       {
         path: "" , component: AdminLayoutComponent, children: [
@@ -34,6 +37,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     ])
   ],
   exports: [RouterModule],
+  providers: [ AuthService ]
 })
 export class AdminModule {
 
--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {User} from "../../shared/interfaces";
+import {AuthService} from "../shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -11,7 +13,10 @@ export class LoginPageComponent implements OnInit {
 
   public form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) { }
 
   public ngOnInit(): void {
      this.form = new FormGroup({
@@ -34,5 +39,10 @@ export class LoginPageComponent implements OnInit {
         password: this.form.value.password,
       }
 
+      this.auth.login(user)
+        .subscribe(() => {
+          this.form.reset();
+          this.router.navigate(["/admin", "dashboard"]);
+        });
   }
 }
