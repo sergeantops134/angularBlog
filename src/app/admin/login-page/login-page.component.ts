e73fd220fe0804edbd102f1a429de85dcce8eceb
Olenych <p.olenych@andersenlab.com>
2021-10-22 11:48:21 +0300

    L-013 #comment Added guards for admin routes

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {User} from "../../shared/interfaces";
 import {AuthService} from "../shared/services/auth.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -12,14 +12,22 @@ import {Router} from "@angular/router";
 export class LoginPageComponent implements OnInit {
 
   public form: FormGroup;
-  public submitted = false;
+  public submitted: boolean = false;
+  public message: string;
 
   constructor(
     public auth: AuthService,
     private router: Router,
+    private route: ActivatedRoute,
   ) { }
 
   public ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params["loginAgain"]) {
+        this.message = "Please login"
+      }
+    });
+
      this.form = new FormGroup({
        email: new FormControl(null, [
          Validators.required,
@@ -29,7 +37,7 @@ export class LoginPageComponent implements OnInit {
          Validators.required,
          Validators.minLength(6),
        ]),
-     })
+     });
   }
 
   public submit(): void {
